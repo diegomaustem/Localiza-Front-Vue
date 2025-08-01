b1f0bb13d808e54b02d864165984db0a043bfa79
diegomaustem <diego.maustem@gmail.com>
2025-08-01 13:26:44 -0300

    :wrench: chore: Set configurations of main

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import "./style.css";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import router from "./router/routes";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+createApp(App).use(router).mount("#app");

5e8a0d1f9022dbfa8ed88457d84fafc5deedf2bb
diegomaustem <diego.maustem@gmail.com>
2025-08-05 11:06:12 -0300

    :sparkles: feat: Adding sweetalert in project global

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { createApp } from "vue";
+import App from "./App.vue";
 import "./style.css";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import router from "./router/routes";
-import App from "./App.vue";
+import Swal from "sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+app.use(router);
+app.config.globalProperties.$swal = Swal;
+app.mount("#app");
